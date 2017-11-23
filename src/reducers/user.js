e0f8d4e7f13eaa7cca3ae25814da07f9114a7e8c
fairray <fairray@syneforge.com>
2017-11-23 19:51:24 +0200

    clean user reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,24 +1,16 @@
 import { USER_GET_SUCCESS, USER_GET_FAILER } from '../constants/user';
 
-const initialState = {
-    name: null,
-    role: 'guest',
-};
+const initialState = {};
 
 export default(state = initialState, action) => {
     switch (action.type) {
         case USER_GET_SUCCESS :
             return {
                 ...state,
-                name: action.payload.name,
-                role: action.payload.role,
+                ...action.payload
             };
         case USER_GET_FAILER :
-            return {
-                ...state,
-                name: null,
-                role: 'guest',
-            };
+            return {};
         default:
             return state;
     }
